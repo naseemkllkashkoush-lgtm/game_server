e4a5b389bf2d5d1a9e44562a36de6023e202c752
naseemkllkashkoush-lgtm <naseemkllkashkoush@gmail.com>
2026-03-14 12:40:11 +0200

    test

--- api/route/product.route.js
+++ api/route/product.route.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { createProduct, findAllProduct } = require("../controllers/product.controller");
+const { createProduct, findAllProduct, deleteProduct } = require("../controllers/product.controller");
 
 const productRout = express.Router();
 
 productRout.post("/createproduct", createProduct)
 productRout.post("/findAllProduct", findAllProduct)
-
+productRout.delete("/deleteProduct", deleteProduct)
 
 module.exports = productRout;
